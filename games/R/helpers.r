09f18f2d5a06e47633df1b720515fbdfba17f97f
Brenton Kenkel <brenton.kenkel@gmail.com>
2010-11-15 13:27:50 -0500

    Add function to get best starting values from log-likelihood profile

--- games/R/helpers.r
+++ games/R/helpers.r
@@ -168,3 +168,16 @@ makeUtils <- function(b, regr, nutils, unames, finit = TRUE)
     utils$b <- b
     return(utils)
 }
+
+##
+## Retrieves the "best" starting values from profile.game output
+##
+svalsFromProfile <- function(x)
+{
+    x <- do.call(rbind, x)
+    bestrow <- which.max(x[, 1])  ## highest log-likelihood
+    xn <- names(x)[-1]
+    ans <- as.numeric(x[bestrow, ][-1])
+    names(ans) <- xn
+    return(ans)
+}
