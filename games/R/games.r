9eec8eb0e3a3168e0c2d1571f6b5a1918da78848
Brenton Kenkel <brenton.kenkel@gmail.com>
2010-12-29 14:39:03 -0500

    Move non-nested functions to own file

--- games/R/games.r
+++ games/R/games.r
@@ -924,59 +924,7 @@ LW <- function(x)
     return(W4)
 }
 
-##
-## Individual log-likelihoods for a model of class "game", "glm", "vglm"
-## (multinomial logit via VGAM or Zelig), "polr" (ordered logit/probit via MASS
-## or Zelig)
-##
-## The "outcome" argument is for comparing a strategic model to a binary logit
-## or probit model where the response is one of the possible outcomes (see the
-## examples for the "vuong"/"clarke" functions)
-## 
-indivLogLiks <- function(model, outcome)
-{
-    ## need to deal with weights?
-
-    ## change how it works for the ultimatum model?  model$y == outcome will
-    ## give the wrong results.  OTOH if only outcome of interest is the offer,
-    ## that should specified -- perhaps ask curt?
-
-    ## add multinomial logit from package mlogit!
-    
-    if (inherits(model, "game") && is.null(outcome)) {
-        ans <- model$log.likelihood
-    } else if (inherits(model, "game")) {
-        pp <- predict(model)[, outcome]
-        ans <- ifelse(as.numeric(model$y) == outcome, pp, 1 - pp)
-        ans <- log(ans)
-    } else if (inherits(model, "glm")) {
-        ans <- ifelse(model$y == 1, fitted.values(model),
-                      1 - fitted.values(model))
-        ans <- log(ans)
-    } else if (inherits(model, "vglm") && "multinomial" %in%
-               model@family@vfamily) {
-        y <- apply(model@y, 1, function(x) which(x == 1))
-        pp <- fitted.values(model)
-        pp <- pp[cbind(1:nrow(pp), y)]  # gets the y[i]'th entry from the i'th
-                                        # row of pp
-        ans <- log(pp)
-    } else if (inherits(model, "polr")) {
-        y <- as.numeric(model.response(model$model))
-        pp <- fitted.values(model)
-        pp <- pp[cbind(1:nrow(pp), y)]
-        ans <- log(pp)
-    } else {
-        stop("model is not of a supported class")
-    }
-
-    return(ans)
-}
-
-vuong <- function(model1, model2, outcome1 = NULL, outcome2 = NULL)
-{
-}
-
-##' .. content for description (no empty lines) ..
+##' Calculate profile likelihood to assess convergence of a model.
 ##'
 ##' Likelihood profiling can help determine if a model fit failed to reach a
 ##' global maximum, which can be an issue (especially for the
