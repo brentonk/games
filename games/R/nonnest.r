a9e488a3b52dfb2d1ebbd1129649a2d62b2fd6ee
Brenton Kenkel <brenton.kenkel@gmail.com>
2011-01-13 22:27:29 -0500

    About to get rid of polr and vglm functionality, too buggy

--- games/R/nonnest.r
+++ games/R/nonnest.r
@@ -5,11 +5,11 @@ NULL
 ##' .. content for description (no empty lines) ..
 ##'
 ##' .. content for details ..
-##' @title 
-##' @param x 
-##' @param digits 
-##' @param ... 
-##' @return 
+##' @title print
+##' @param x wef
+##' @param digits qed
+##' @param ... qewd
+##' @return qdw
 ##' @author Brenton Kenkel
 print.nonnest.test <- function(x, digits = 2, ...)
 {
@@ -86,6 +86,8 @@ indivLogLiks <- function(model, outcome = NULL)
         ans <- dnorm(msum$residuals, sd = msum$sigma, log = TRUE)
     } else if (inherits(model, "vglm") &&
                "multinomial" %in% model@family@vfamily) {
+        if (!all(model@y %in% c(0, 1)))
+            stop("multinomial model must be specified in terms of separate observations, not counts")
         y <- apply(model@y, 1, function(x) which(x == 1))
         pp <- fitted.values(model)
         pp <- pp[cbind(1:nrow(pp), y)]  # gets the y[i]'th entry from the i'th
@@ -93,6 +95,8 @@ indivLogLiks <- function(model, outcome = NULL)
         ans <- log(pp)
     } else if (inherits(model, "polr")) {
         y <- as.numeric(model.response(model$model))
+        if (length(y) != model$nobs)
+            stop("polr model must be specified in terms of separate observations, not counts")
         pp <- fitted.values(model)
         pp <- pp[cbind(1:nrow(pp), y)]
         ans <- log(pp)
@@ -109,7 +113,13 @@ nparams <- function(model)
 
     if (inherits(model, "game")) {
         ans <- sum(!model$fixed)
-    } else {
+    } else if (inherits(model, "polr")) {
+        ans <- model$edf
+    } else if (inherits(model, "vglm")) {
+        
+    } else if (inherits(model, "glm")) {
+        ans <- attr(logLik(model), "df")
+    } else if (inherits(model, "lm")) {
         ans <- length(coef(model))
     }
 
@@ -124,31 +134,23 @@ nobs <- function(model)
         ans <- nrow(model$model)
     } else if (inherits(model, c("lm", "glm"))) {
         ans <- length(model$residuals)
+    } else if (inherits(model, "polr")) {
+        ans <- model$n
     }
 
     return(ans)
 }
 
-##' .. content for description (no empty lines) ..
-##'
-##' .. content for details ..
-##' @title
-##' @aliases vuong clarke
-##' @param model1 
-##' @param model2 
-##' @param outcome1 
-##' @param outcome2 
-##' @param level 
-##' @return
-##' @export
-##' @author Brenton Kenkel (\email{brenton.kenkel@@gmail.com})
-vuong <- function(model1, model2, outcome1 = NULL, outcome2 = NULL,
-                  level = 0.05)
+##
+## setup and error checking for clarke and vuong
+## 
+nonnest <- function(model1, model2, outcome1, outcome2)
 {
-    ## be sure to note no checking for equal order, etc etc
+    if (!inherits(model1, "game") && !is.null(outcome1))
+        warning("model1 is not of class \"game\", so outcome1 will be ignored")
+    if (!inherits(model2, "game") && !is.null(outcome2))
+        warning("model2 is not of class \"game\", so outcome2 will be ignored")
 
-    ## clarke and vuong should have combined help page
-    
     n <- nobs(model1)
     if (nobs(model2) != n)
         stop("model1 and model2 must have same number of observations")
@@ -158,20 +160,62 @@ vuong <- function(model1, model2, outcome1 = NULL, outcome2 = NULL,
     p1 <- nparams(model1)
     p2 <- nparams(model2)
 
-    correction <- (p1 - p2) * (log(n) / 2)
-    num <- sum(loglik1) - sum(loglik2) - correction
-    denom <- sd(loglik1 - loglik2) * sqrt(n / (n-1))  # correcting for R's use
-                                                      # of n-1 denominator in sd
-                                                      # calculation
-    stat <- num / (sqrt(n) * denom)
+    return(list(n = n, loglik1 = loglik1, loglik2 = loglik2, p1 = p1, p2 = p2))
+}
+
+##' .. content for description (no empty lines) ..
+##'
+##' .. content for details ..
+##' @title Non-nested model tests
+##' @aliases vuong clarke
+##' @param model1 A fitted statistical model (see "Details" for the types of
+##' models available)
+##' @param model2 A fitted statistical model
+##' @param outcome1 Optional: if \code{model1} is of class \code{"game"},
+##' specify an integer to restrict attention to a particular binary outcome (the
+##' corresponding column of \code{predict(model1)}).  See "Details" below for
+##' more information on when to specify an outcome.  If \code{model1} is not of
+##' class \code{"game"} and \code{outcome1} is non-\code{NULL}, it will be
+##' ignored and a warning will be issued.
+##' @param outcome2 Optional: same as \code{outcome1}, but corresponding to
+##' \code{model2}.
+##' @param level Numeric: significance level for the test.
+##' @references (cite vuong and kevin!)
+##' @return An object of class \code{"nonnest.test"}, which is a list
+##' containing: \describe{
+##' \item{\code{stat}}{The test statistic}
+##' \item{\code{test}}{The type of test (\code{"vuong"} or \code{"clarke"})}
+##' \item{\code{level}}{Significance level for the test}
+##' \item{\code{loglik1}}{Vector of observationwise log-likelihoods for
+##' \code{model1}}
+##' \item{\code{loglik2}}{Vector of observationwise log-likelihoods for
+##' \code{model2}}
+##' \item{\code{nparams}}{Integer vector containing the number of parameters
+##' fitted in \code{model1} and \code{model2} respectively}
+##' \item{\code{nobs}}{Number of observations of the dependent variable being
+##' modeled}}
+##' @seealso \code{\link{print.nonnest.test}}
+##' @export
+##' @author Brenton Kenkel (\email{brenton.kenkel@@gmail.com})
+vuong <- function(model1, model2, outcome1 = NULL, outcome2 = NULL,
+                  level = 0.05)
+{
+    x <- nonnest(model1, model2, outcome1, outcome2)
+
+    correction <- (x$p1 - x$p2) * (log(x$n) / 2)
+    num <- sum(x$loglik1) - sum(x$loglik2) - correction
+    denom <- sd(x$loglik1 - x$loglik2) *
+        sqrt(x$n / (x$n-1))  # correcting for R's use of n-1 denominator in sd
+                             # calculation
+    stat <- num / (sqrt(x*n) * denom)
 
     ans <- list(stat = stat,
                 test = "vuong",
                 level = level,
-                loglik1 = loglik1,
-                loglik2 = loglik2,
-                nparams = c(p1, p2),
-                nobs = n)
+                loglik1 = x$loglik1,
+                loglik2 = x$loglik2,
+                nparams = c(x$p1, x$p2),
+                nobs = x$n)
     class(ans) <- "nonnest.test"
 
     return(ans)
@@ -180,25 +224,18 @@ vuong <- function(model1, model2, outcome1 = NULL, outcome2 = NULL,
 clarke <- function(model1, model2, outcome1 = NULL, outcome2 = NULL,
                    level = 0.05)
 {
-    n <- nobs(model1)
-    if (nobs(model2) != n)
-        stop("model1 and model2 must have same number of observations")
-    
-    loglik1 <- indivLogLiks(model1, outcome1)
-    loglik2 <- indivLogLiks(model2, outcome2)
-    p1 <- nparams(model1)
-    p2 <- nparams(model2)
+    x <- nonnest(model1, model2, outcome1, outcome2)
 
-    correction <- (p1 - p2) * (log(n) / (2*n))
-    stat <- sum(loglik1 - loglik2 > correction)
+    correction <- (x$p1 - x$p2) * (log(x$n) / (2*x$n))
+    stat <- sum(x$loglik1 - x$loglik2 > correction)
 
     ans <- list(stat = stat,
                 test = "clarke",
                 level = level,
-                loglik1 = loglik1,
-                loglik2 = loglik2,
-                nparams = c(p1, p2),
-                nobs = n)
+                loglik1 = x$loglik1,
+                loglik2 = x$loglik2,
+                nparams = c(x$p1, x$p2),
+                nobs = x$n)
     class(ans) <- "nonnest.test"
 
     return(ans)
