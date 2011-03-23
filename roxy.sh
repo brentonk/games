8004cabb8fa34cc658e2c4754b60b4e2bfeb31f5
Brenton Kenkel <brenton.kenkel@gmail.com>
2011-03-23 10:26:57 -0400

    keep heckprob files out of built package for now

--- roxy.sh
+++ roxy.sh
@@ -2,17 +2,22 @@
 
 rm -rf ./games.roxygen
 
+mv games/R/heckprob.r games/R/.heckprob.bak
+
 /usr/bin/R --no-save <<EOF
 library(roxygen)
 roxygenize("games", use.Rd2 = TRUE)
 q()
 EOF
 
+mv games/R/.heckprob.bak games/R/heckprob.r
+
 rmdir games.roxygen/inst/doc
 rmdir games.roxygen/inst
 rm -f games.roxygen/.Rhistory
 rm -f games.roxygen/.Rhistorynew
 rm -f games.roxygen/R/.Rhistory
+rm -f games.roxygen/R/.heckprob.bak
 
 # for including S3 method directives for undocumented methods
 cat ./games/NAMESPACE >> ./games.roxygen/NAMESPACE
